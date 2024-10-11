c48ceb8928cf2c058209a33b17f1e426ea0b1a21
Ian Pittwood <ian.pittwood@rstudio.com>
2024-10-11 12:18:18 -0600

    First attempt at custom TOML configs

---
+++ posit-bakery/posit_bakery/bake/parser/filters.py
@@ -0,0 +1,28 @@
+import re
+
+import jinja2
+
+
+def tag_safe(s: str):
+    return s.replace("+", "-")
+
+
+def clean_version(s: str):
+    return re.sub(r"[+|-]", "", s)
+
+
+def condense_os(s: str):
+    return s.replace(" ", "").replace("-", "").replace(".", "").lower()
+
+
+def jinja2_env():
+    env = jinja2.Environment()
+    env.filters["tag_safe"] = tag_safe
+    env.filters["clean_version"] = clean_version
+    env.filters["condense_os"] = condense_os
+    return env
+
+
+def render_template(template: str, **kwargs):
+    template = jinja2_env().from_string(template)
+    return template.render(**kwargs)
---
+++ posit-bakery/posit_bakery/bake/parser/manifest.py
@@ -0,0 +1,163 @@
+import tomllib
+from pathlib import Path
+from typing import Dict
+
+import jinja2
+from rich import print
+
+from posit_bakery.bake.parser.filters import jinja2_env, render_template
+
+
+class TargetBuild:
+    def __init__(self, manifest_name: str, build: Dict, target: Dict, manifest_path: Path, const: Dict = None):
+        if const is None:
+            const = {}
+
+        self.image_name = const.get("image_name", manifest_name)
+        self.build_data = build
+        self.target_data = target
+
+        self.version = build["version"]
+        self.type = target["type"]
+
+        # Resolve a unique name to be used in Bake plans
+        self.name = (
+            f"{manifest_name}-{self.version}-{self.type}".replace(".", "-").replace("/","-")
+        )
+
+        self.latest = build.get("latest", False)
+
+        # Resolve operating system type either via build array (product) or constants (base)
+        self.os = None
+        default_tags = ["{{ build.version }}"]
+        default_latest_tags = []
+        if self.build_data.get("os"):
+            self.os = self.build_data["os"]
+            self.containerfile_name = render_template(
+                build.get("containerfile", "Containerfile.{{ build.os | condense_os }}.{{ target.type }}"),
+                build=build,
+                target=target,
+                **const,
+            )
+            self.containerfile_path = manifest_path / self.version / self.containerfile_name
+
+            if self.type == "std":
+                default_tags = [
+                    "{{ build.version | tag_safe }}-{{ build.os | condense_os }}",
+                    "{{ build.version | clean_version }}-{{ build.os | condense_os }}",
+                ]
+                if self.latest:
+                    default_latest_tags.append("{{ build.os | condense_os }}")
+                    if self.build_data.get("primary_os"):
+                        default_latest_tags.append("latest")
+            else:
+                default_tags = ["{{ build.version }}-{{ target.type }}"]
+                if self.latest:
+                    default_latest_tags = ["{{ build.os | condense_os }}-{{ target.type }}"]
+                    if self.build_data.get("primary_os"):
+                        default_latest_tags.append("latest-{{ target.type }}")
+        elif const.get("os"):
+            self.os = f"{const['os'].title()} {self.version}"
+            self.containerfile_name = render_template(build.get("containerfile", f"Containerfile.{self.type}"), build=build, target=target, **const)
+            self.containerfile_path = manifest_path / self.version / self.containerfile_name
+
+            if self.type == "std":
+                default_tags = ["{{ os | lower }}{{ build.version | condense_os }}"]
+                if self.latest:
+                    default_latest_tags = ["{{ os | lower }}{{ build.version | condense_os }}-latest"]
+            else:
+                default_tags = ["{{ build.version }}-{{ target.type }}"]
+                if self.latest:
+                    default_latest_tags = ["latest-{{ target.type }}"]
+        else:
+            print(
+                f"[bright_yellow bold]WARNING:[/bold] No operating system could be determined for manifest {self.name}."
+                f" This can result in unexpected behavior."
+            )
+
+        # Render tags
+        self.tags = []
+        tag_list = target.get("tags", default_tags)
+        for tag in tag_list:
+            self.tags.append(render_template(tag, build=build, target=target, **const))
+        if self.latest:
+            for tag in target.get("latest_tags", default_latest_tags):
+                self.tags.append(render_template(tag, build=build, target=target, **const))
+
+        # Render Goss settings or set to defaults
+        goss_data = target.get("goss")
+        if goss_data:
+            self.goss_deps = Path(
+                render_template(goss_data.get("deps", f"{self.version}/deps"), build=build, target=target, **const)
+            )
+            self.goss_deps = manifest_path / self.goss_deps
+
+            self.goss_test_path = Path(
+                render_template(goss_data.get("path", f"{self.version}/tests"), build=build, target=target, **const)
+            )
+            self.goss_test_path = manifest_path / self.goss_test_path
+
+            self.goss_wait = goss_data.get("wait", 0)
+
+            self.goss_command = render_template(
+                goss_data.get("command", "sleep infinity"), build=build, target=target, **const
+            )
+        else:
+            self.goss_deps = manifest_path / f"{self.version}/deps"
+            self.goss_test_path = manifest_path / f"{self.version}/tests"
+            self.goss_wait = 0
+            self.goss_command = "sleep infinity"
+
+    def render_fq_tags(self, host_url: str):
+        return [f"{host_url}/{self.image_name}:{tag}" for tag in self.tags]
+
+
+class Manifest:
+    def __init__(self, name: str, manifest_file: Path):
+        self.name = name
+        self.manifest_file = manifest_file
+        self.manifest_path = manifest_file.parent
+        with open(self.manifest_file, "rb") as f:
+            self.manifest_config = tomllib.load(f)
+        self.targets_data = self.get_targets()
+        self.builds_data = self.get_builds()
+        self.target_builds = []
+        for build in self.builds_data.values():
+            for target_type, target in self.targets_data.items():
+                build_targets = build.get("targets", [])
+                if build_targets.empty() or target_type in build_targets:
+                    self.target_builds.append(TargetBuild(self.name, build, target, self.manifest_path))
+                else:
+                    print(f"[bright_black]Skipping target '{target_type}' for build version '{build['version']}'")
+
+    def load_manifest(self):
+        with open(self.manifest_file, "rb") as f:
+            self.manifest_config = tomllib.load(f)
+
+    def get_targets(self):
+        targets = {}
+        for target in self.manifest_config["target"]:
+            targets[target["type"]] = target
+        return targets
+
+    def get_builds(self):
+        builds = {}
+        for build in self.manifest_config["build"]:
+            if "os" in build:
+                os_list = build["os"].copy()
+                primary_os = os_list[0]
+                for os in os_list:
+                    build["os"] = os
+                    build["primary_os"] = os == primary_os
+                    builds[build["version"] + "-" + os] = build
+            else:
+                builds[build["version"]] = build
+        return builds
+
+
+def load_manifests_from_context(context: Path):
+    manifests = {}
+    for manifest_file in context.rglob("manifest.toml"):
+        manifest_name = str(manifest_file.parent.relative_to(context))
+        manifests[manifest_name] = Manifest(manifest_name, manifest_file)
+    return manifests
