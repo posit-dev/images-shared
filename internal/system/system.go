79c65a8ab156ad6cca8a326eed801b7ed58f559d
Ian Pittwood <ian.pittwood@rstudio.com>
2024-10-29 12:33:56 -0600

    Move command execution to its own function

--- internal/system/system.go
+++ internal/system/system.go
@@ -1,11 +1,34 @@
 package system
 
 import (
+	"fmt"
 	"github.com/zcalusic/sysinfo"
 	"log"
+	"log/slog"
+	"os"
 	"os/exec"
 	"os/user"
 )
+
+func RunCommand(command string, args *[]string, envVars *[]string) error {
+	if args == nil {
+		args = &[]string{}
+	}
+	cmd := exec.Command(command, *args...)
+	cmd.Env = os.Environ()
+	if envVars != nil {
+		cmd.Env = append(cmd.Env, *envVars...)
+	}
+	// TODO: Consider a way to suppress output
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	slog.Debug(fmt.Sprintf("Environment variables: %v", cmd.Env))
+	slog.Debug("Running command: " + cmd.String())
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+	return nil
+}
 
 func RequireSudo() {
 	current, err := user.Current()
@@ -24,11 +47,11 @@ func UpdateCACertificates() error {
 
 	switch si.OS.Vendor {
 	case "ubuntu", "debian":
-		if err := exec.Command("update-ca-certificates").Run(); err != nil {
+		if err := RunCommand("update-ca-certificates", nil, nil); err != nil {
 			return err
 		}
 	case "almalinux", "centos", "rockylinux", "rhel":
-		if err := exec.Command("update-ca-trust").Run(); err != nil {
+		if err := RunCommand("update-ca-trust", nil, nil); err != nil {
 			return err
 		}
 	}
