e8edc34da07be0c80bddca58e24eb3cc1860070c
Ian Pittwood <ian.pittwood@rstudio.com>
2024-11-08 15:17:51 -0700

    Convert config and manifest to Pydantic models

--- posit-bakery/posit_bakery/parser/manifest.py
+++ posit-bakery/posit_bakery/parser/manifest.py
@@ -1,213 +1,218 @@
 import os
 import re
 
-import tomlkit
 from pathlib import Path
-from typing import Dict, Union, List, Any
+from typing import Dict, Union, List, Any, Set
 
-from rich import print
+import jinja2
+from pydantic import model_validator
+from pydantic.dataclasses import dataclass
+from pydantic_core import ArgsKwargs
 from tomlkit import TOMLDocument
-from tomlkit.items import AoT, Table
+from tomlkit.items import Table
 
-from posit_bakery.parser.templating.filters import render_template
+from posit_bakery.error import BakeryConfigError, BakeryFileNotFoundError
+from posit_bakery.parser.config import Config
+from posit_bakery.parser.generic import GenericTOMLModel
+from posit_bakery.parser.templating.filters import render_template, condense, tag_safe, clean_version, jinja2_env
 
 
+@dataclass
+class GossConfig:
+    version_context: Union[str, bytes, os.PathLike]
+    build_data: Dict[str, Any]
+    target_data: Dict[str, Any]
+    const: Dict[str, Any] = None
+
+    deps: Path = None
+    test_path: Path = None
+    wait: int = 0
+    command: str = "sleep infinity"
+
+    def __post_init__(self):
+        if self.deps is None:
+            self.deps = Path(self.version_context) / "deps"
+        if self.test_path is None:
+            self.test_path = Path(self.version_context) / "test"
+
+        if self.deps:
+            self.deps = Path(
+                render_template(str(self.deps), build=self.build_data, target=self.target_data, **self.const)
+            )
+        if self.test_path:
+            self.test_path = Path(
+                render_template(str(self.test_path), build=self.build_data, target=self.target_data, **self.const)
+            )
+        if self.wait:
+            self.wait = int(render_template(str(self.wait), build=self.build_data, target=self.target_data, **self.const))
+        if self.command:
+            self.command = render_template(self.command, build=self.build_data, target=self.target_data, **self.const)
+
+
+@dataclass
 class TargetBuild:
-    def __init__(
-            self,
-            manifest_name: str,
-            build: Union[Table, Dict[str, Any]],
-            target: Union[Table, Dict[str, Any]],
-            manifest_path: Union[str, bytes, os.PathLike],
-            const: Union[Table, Dict[str, Any]] = None
-    ):
-        # Default const to an empty dictionary
-        if const is None:
-            const = {}
+    manifest_context: Union[str, bytes, os.PathLike]
+    config: Config
+    build_data: Dict[str, Any]
+    target_data: Dict[str, Any]
 
-        # Resolve image name
-        self.image_name: str = const.get("image_name", manifest_name)
+    image_name: str
+    version: str
+    type: str
 
-        # Save build and target data
-        self.build_data: Union[Table, Dict[str, Any]] = build
-        self.target_data: Union[Table, Dict[str, Any]] = target
+    uid: str = None
+    const: Dict[str, Any] = None
+    os: str = None
+    containerfile: str = None
+    containerfile_path: Path = None
+    latest: bool = False
+    primary_os: bool = False
+    tags: List[str] = None
+    latest_tags: List[str] = None
+    goss: GossConfig = None
 
-        self.version: str = build["version"]
-        self.type: str = target["type"]
+    @property
+    def all_tags(self) -> List[str]:
+        tags = []
+        for registry in self.config.registry:
+            for tag in self.tags:
+                tags.append(f"{registry.base_url}/{self.image_name}:{tag}")
+            if self.latest:
+                for tag in self.latest_tags:
+                    tags.append(f"{registry.base_url}/{self.image_name}:{tag}")
+        return tags
 
-        # Resolve a unique name to be used in Bake plans
-        self.name: str = re.sub("[.+/]", "-", f"{manifest_name}-{self.version}-{self.type}")
-
-        self.latest: bool = build.get("latest", False)
-
-        # Resolve operating system type either via build array (product) or constants (base)
-        self.os = None
-        default_tags = ["{{ build.version }}"]
-        default_latest_tags = []
-        # Logic branch for product builds where OSes are non-homogenous
-        if self.build_data.get("os"):
-            self.os: str = self.build_data["os"]
-            self.containerfile_name: str = render_template(
-                build.get("containerfile", "Containerfile.{{ build.os | condense }}.{{ target.type }}"),
-                build=build,
-                target=target,
-                **const,
+    @model_validator(mode="before")
+    @classmethod
+    def pre_root(cls, values: ArgsKwargs) -> ArgsKwargs:
+        if "goss" in values.kwargs:
+            version_context = values.kwargs["manifest_context"] / values.kwargs["version"]
+            const = values.kwargs.get("const")
+            if const is None:
+                const = {"image_name": values.kwargs["image_name"]}
+            values.kwargs["goss"] = GossConfig(
+                version_context=version_context,
+                build_data=values.kwargs["build_data"],
+                target_data=values.kwargs["target_data"],
+                const=const,
+                **values.kwargs["goss"]
             )
-            self.containerfile_path: Path = Path(manifest_path) / self.version / self.containerfile_name
+        return values
 
-            if self.type == "std":
-                default_tags = [
-                    "{{ build.version | tag_safe }}-{{ build.os | condense }}",
-                    "{{ build.version | clean_version }}-{{ build.os | condense }}",
-                ]
-                if self.latest:
-                    default_latest_tags.append("{{ build.os | condense }}")
-                    if self.build_data.get("primary_os"):
-                        default_latest_tags.append("latest")
-            else:
-                default_tags = [
-                    "{{ build.version | tag_safe }}-{{ build.os | condense }}-{{ target.type }}",
-                    "{{ build.version | clean_version }}-{{ build.os | condense }}-{{ target.type }}",
-                ]
-                if self.latest:
-                    default_latest_tags = ["{{ build.os | condense }}-{{ target.type }}"]
-                    if self.build_data.get("primary_os"):
-                        default_latest_tags.append("latest-{{ target.type }}")
-        # Logic for base images where OS is homogenous
-        elif const.get("os"):
-            self.os: str = f"{const['os'].title()} {self.version}"
-            self.containerfile_name: str = render_template(
-                build.get("containerfile", f"Containerfile.{self.type}"), build=build, target=target, **const
-            )
-            self.containerfile_path: Path = Path(manifest_path) / self.version / self.containerfile_name
+    def __post_init__(self):
+        if self.uid is None:
+            self.uid = re.sub("[.+/]", "-", f"{self.image_name}-{self.version}-{self.type}")
 
-            if self.type == "std":
-                default_tags = ["{{ os | lower }}{{ build.version | condense }}"]
-                if self.latest:
-                    default_latest_tags = ["{{ os | lower }}{{ build.version | condense }}-latest"]
-            else:
-                default_tags = ["{{ build.version }}-{{ target.type }}"]
-                if self.latest:
-                    default_latest_tags = ["latest-{{ target.type }}"]
-        # The "best effort" path where no OS is defined
+        if self.const is None:
+            self.const = {"image_name": self.image_name}
+        if "image_name" not in self.const:
+            self.const["image_name"] = self.image_name
+
+        if self.os is None and "os" in self.const:
+            self.os = self.const["os"]
+        elif self.os is None:
+            raise ValueError(f"No operating system could be determined for manifest '{self.uid}'.")
+
+        if self.containerfile is None:
+            condensed_os = condense(self.os)
+            potential_names = [f"Containerfile.{condensed_os}.{self.type}", f"Containerfile.{self.type}", "Containerfile"]
+            for name in potential_names:
+                potential_path = Path(self.manifest_context) / self.version / name
+                if potential_path.exists():
+                    self.containerfile = name
+                    self.containerfile_path = potential_path
+                    break
         else:
-            print(
-                f"[bright_yellow][bold]WARNING:[/bold] No operating system could be determined for manifest {self.name}."
-                f" This can result in unexpected behavior."
+            self.containerfile = render_template(
+                self.containerfile, build=self.build_data, target=self.target_data, **self.const
             )
+            self.containerfile_path = Path(self.manifest_context) / self.version / self.containerfile
 
-        # Render tags
-        self.tags: List[str] = []
-        tag_list = target.get("tags", default_tags)
-        for tag in tag_list:
-            self.tags.append(render_template(tag, build=build, target=target, **const))
-        if self.latest:
-            for tag in target.get("latest_tags", default_latest_tags):
-                self.tags.append(render_template(tag, build=build, target=target, **const))
-
-        # Render Goss settings or set to defaults
-        goss_data = target.get("goss")
-        if goss_data:
-            self.goss_deps: Path = Path(
-                render_template(goss_data.get("deps", f"{self.version}/deps"), build=build, target=target, **const)
-            )
-            self.goss_deps: Path = Path(manifest_path) / self.goss_deps
-
-            self.goss_test_path: Path = Path(
-                render_template(goss_data.get("path", f"{self.version}/test"), build=build, target=target, **const)
-            )
-            self.goss_test_path: Path = Path(manifest_path) / self.goss_test_path
-
-            self.goss_wait: int = int(goss_data.get("wait", 0))
-
-            self.goss_command: str = render_template(
-                goss_data.get("command", "sleep infinity"), build=build, target=target, **const
-            )
+        if self.tags is None or len(self.tags) == 0:
+            if self.type == "std":
+                self.tags = [
+                    f"{tag_safe(self.version)}-{condense(self.os)}",
+                    f"{clean_version(self.version)}-{condense(self.os)}",
+                ]
+                if self.primary_os or "os" in self.const:
+                    self.tags.append(f"{tag_safe(self.version)}")
+            else:
+                self.tags = [
+                    f"{tag_safe(self.version)}-{condense(self.os)}-{self.type}",
+                    f"{clean_version(self.version)}-{condense(self.os)}-{self.type}",
+                ]
+                if self.primary_os or "os" in self.const:
+                    self.tags.append(f"{tag_safe(self.version)}-{self.type}")
         else:
-            self.goss_deps: Path = Path(manifest_path) / f"{self.version}/deps"
-            self.goss_test_path: Path = Path(manifest_path) / f"{self.version}/test"
-            self.goss_wait: int = 0
-            self.goss_command: str = "sleep infinity"
+            self.tags = [render_template(tag, build=self.build_data, target=self.target_data, **self.const) for tag in self.tags]
 
-    def render_fq_tags(self, host_url: str) -> List[str]:
-        """Renders the fully qualified tags for the target build"""
-        return [f"{host_url}/{self.image_name}:{tag}" for tag in self.tags]
+        if self.latest_tags is None or len(self.latest_tags) == 0:
+            if self.type == "std":
+                self.latest_tags = [f"{condense(self.os)}", "latest"]
+            else:
+                self.latest_tags = [f"{condense(self.os)}-{self.type}", f"latest-{self.type}"]
+        else:
+            self.latest_tags = [render_template(tag, build=self.build_data, target=self.target_data, **self.const) for tag in self.latest_tags]
+
+        if self.goss is None:
+            self.goss = GossConfig(
+                version_context=self.manifest_context / self.version,
+                build_data=self.build_data,
+                target_data=self.target_data,
+                const=self.const
+            )
+
+    def __hash__(self):
+        return hash(self.uid)
 
 
-class Manifest:
-    def __init__(
-            self,
-            context: Union[str, bytes, os.PathLike],
-            name: str,
-            manifest_file: Union[str, bytes, os.PathLike],
-            filters: Dict[str, str] = None,
-    ):
-        # Default filters to an empty dictionary
-        if filters is None:
-            filters = {}
-
-        # Set manifest name and load manifest file
-        self.name: str = name
-        self.manifest_file: Path = Path(manifest_file)
-        self.manifest_file_relative: Path = self.manifest_file.relative_to(Path(context))
-        self.manifest_config: TOMLDocument = self.load(self.manifest_file)
-
-        # Load manifest data
-        # Get constants from the manifest or default to empty dictionary
-        self.const: Union[Table, Dict[str, Any]] = self.manifest_config.get("const", {})
-        # Load target and build data structures
-        self.targets_data: Dict[str, Dict[str, Any]] = self.get_targets()
-        self.builds_data: Dict[str, Dict[str, Any]] = self.get_builds()
-        # Initialize list of target builds
-        self.target_builds: List[TargetBuild] = []
-        for build in self.builds_data.values():
-            for target_type, target in self.targets_data.items():
-                # Pair targets and builds together into TargetBuild objects
-                build_targets = build.get("targets", [])
-                if (target_type in build_targets or not build_targets) and (
-                    filters.get("image_version") == build["version"] or filters.get("image_version") is None
-                ):
-                    # Add target if it matches the build's target list or the build does not have a target list
-                    # AND the build is not filtered out by a version filter
-                    self.target_builds.append(
-                        TargetBuild(self.name, build, target, self.manifest_file_relative.parent, self.const)
-                    )
-                else:
-                    print(f"[bright_black]Skipping target '{target_type}' for build version '{build['version']}'")
+class Manifest(GenericTOMLModel):
+    image_name: str
+    manifest_context: Path
+    config: Config
+    target_builds: Set[TargetBuild] = None
 
     @staticmethod
-    def load(manifest_file: Union[str, bytes, os.PathLike]) -> TOMLDocument:
-        """Loads a TOML file into a TOMLDocument object"""
-        with open(manifest_file, "rb") as f:
-            return tomlkit.load(f)
-
-    def get_targets(self) -> Dict[str, Dict[str, Any]]:
-        """Returns a dictionary of target types and their configurations"""
-        targets = {}
-        for target in self.manifest_config["target"]:
-            targets[target["type"]] = target
-        return targets
-
-    def get_builds(self) -> Dict[str, Dict[str, Any]]:
-        """Returns a dictionary of build versions and their configurations"""
-        builds = {}
-        for build in self.manifest_config["build"]:
-            if "os" in build:
-                os_list = build["os"].copy()
-                primary_os = os_list[0]
-                for _os in os_list:
-                    build["os"] = _os
-                    build["primary_os"] = _os == primary_os
-                    builds[build["version"] + "-" + _os] = build
-            else:
-                builds[build["version"]] = build
-        return builds
+    def generate_target_builds(config: Config, manifest_context: Path, manifest_document: TOMLDocument):
+        target_builds = []
+        for build_version, build_data in manifest_document["build"].unwrap().items():
+            os_list = build_data.pop("os", build_data.get("const", {}).pop("os", None))
+            if os_list is None or type(os_list) is str:
+                os_list = [os_list]
+            for _os in os_list:
+                for target_type, target_data in manifest_document["target"].unwrap().items():
+                    target_builds.append(TargetBuild(
+                        manifest_context=manifest_context,
+                        config=config,
+                        build_data=build_data,
+                        target_data=target_data,
+                        image_name=manifest_document["image_name"],
+                        version=build_version,
+                        type=target_type,
+                        os=_os,
+                        const=manifest_document.get("const"),
+                        **build_data,
+                        **target_data,
+                    ))
+        return target_builds
 
     @classmethod
-    def load_manifests_from_context(
-            cls, context: Union[str, bytes, os.PathLike], image_name: str = None, image_version: str = None
-    ) -> Dict[str, "Manifest"]:
-        """Loads all manifests from a context directory
+    def load_file_with_config(cls, config: Config, filepath: Union[str, bytes, os.PathLike]) -> "Manifest":
+        filepath = Path(filepath)
+        d = cls.__load_file_data(filepath)
+        image_name = d.get("image_name")
+        if image_name is None:
+            raise BakeryConfigError(f"Manifest at '{filepath}' does not have an 'image_name' field.")
+        target_builds = cls.generate_target_builds(config, Path(filepath).parent, d)
+        return cls(
+            filepath=filepath,
+            __document=d,
+            context=config.context,
+            image_name=image_name,
+            manifest_context=Path(filepath).parent,
+            config=config,
+            target_builds=target_builds,
+        )
 
         :param context: The context directory
         :param image_name: Filter the manifests loaded by image name
