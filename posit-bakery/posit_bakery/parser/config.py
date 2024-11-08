85d79b492c0e5dcfe129ae8432b2d66c45b36115
Ian Pittwood <ian.pittwood@rstudio.com>
2024-11-08 15:17:51 -0700

    Convert config and manifest to Pydantic models

--- posit-bakery/posit_bakery/parser/config.py
+++ posit-bakery/posit_bakery/parser/config.py
@@ -1,90 +1,64 @@
 import os
 from pathlib import Path
-from typing import Union, List, Dict, Optional
+from typing import Optional, Set, Union
 
-import tomlkit
-from rich import print
-from tomlkit import TOMLDocument
-from tomlkit.container import Container
-from tomlkit.items import AoT
+from pydantic.dataclasses import dataclass
 
-from posit_bakery.error import BakeryConfigError
+from posit_bakery.parser.generic import GenericTOMLModel
 
 
-class Config:
-    def __init__(
-            self,
-            config_file: Union[str, os.PathLike],
-            override_config_file: Union[str, os.PathLike, None] = None,
-    ) -> None:
-        # The config.toml file for the project
-        self.config_file: Path = Path(config_file)
-        if not self.config_file.exists():
-            print(
-                f"[bright_red bold]ERROR:[/bold] No config file found at {self.config_file}. "
-                f"A `config.toml` file is required in the context root."
-            )
-            raise BakeryConfigError(f"No config file found at {self.config_file}")
+@dataclass
+class ConfigRegistry:
+    host: str
+    namespace: Optional[str]
 
-        # Load the config file data
-        self.config_data: TOMLDocument = self.load(self.config_file)
+    @property
+    def base_url(self) -> str:
+        u: str = f"{self.host}"
+        if self.namespace:
+            u = f"{u}/{self.namespace}"
+        return u
 
-        # The registries to push images to
-        self.registries: Union[AoT, List[Dict[str, str]]] = self.config_data.get("registry", None)
-        if not self.registries:
-            raise BakeryConfigError("No registries found in config file")
+    def __hash__(self):
+        return hash(self.base_url + self.namespace)
 
-        # The repository URL for the project, used for labeling purposes
-        self.repository_url: Optional[str] = self.config_data["repository"].get("url", None)
 
-        # The image vendor, used for labeling purposes
-        self.vendor: str = self.config_data["repository"].get("vendor", "Posit Software, PBC")
-        self.maintainer: str = self.config_data["repository"].get("maintainer", "docker@posit.co")
-        self.authors: List[str] = self.config_data["repository"].get("authors", [])
+@dataclass
+class ConfigRepository:
+    authors: Set[str]
+    url: Optional[str]
+    vendor: str = "Posit Software, PBC"
+    maintainer: str = "docker@posit.co"
 
-        if override_config_file is not None:
-            override_config_file = Path(override_config_file)
-            if override_config_file.exists():
-                self.override_config_file: Path = override_config_file
-                self.override_config_data: TOMLDocument = self.load(self.override_config_file)
-                if self.override_config_data.get("registry", None):
-                    self.registries: Union[AoT, List[Dict[str, str]]] = self.override_config_data["registry"]
-                if self.override_config_data.get("repository", None):
-                    override_repository: Container = self.override_config_data["repository"]
-                    self.repository_url = override_repository.get("url", self.repository_url)
-                    self.vendor = override_repository.get("vendor", self.vendor)
-                    self.maintainer = override_repository.get("maintainer", self.maintainer)
-                    self.authors = override_repository.get("authors", self.authors)
 
-    @staticmethod
-    def load(config_file: Union[str, bytes, os.PathLike]) -> TOMLDocument:
-        """Loads a TOML file into a TOMLDocument object
-
-        :param config_file: The path to the TOML file
-        """
-        with open(config_file, "rb") as f:
-            return tomlkit.load(f)
-
-    def get_registry_base_urls(self) -> List[str]:
-        """Returns a list of constructed registry base URLs"""
-        return [f"{r['host']}/{r['namespace']}" for r in self.registries]
+class Config(GenericTOMLModel):
+    registry: Set[ConfigRegistry]
+    repository: ConfigRepository
 
     @classmethod
-    def load_config_from_context(cls, context: Union[str, os.PathLike], skip_override: bool = False) -> "Config":
-        """Loads a Config object from a context directory
+    def load_file(cls, filepath: Union[str, bytes, os.PathLike]) -> "Config":
+        filepath = Path(filepath)
+        d = cls.__load_file_data(filepath)
+        registry = []
+        for r in d["registry"]:
+            registry.append(ConfigRegistry(**r))
+        repository = ConfigRepository(**d["repository"])
+        return cls(
+            filepath=filepath,
+            context=filepath.parent,
+            __document=d,
+            registry=registry,
+            repository=repository
+        )
 
-        :param context: The context directory
-        :param skip_override: Skip loading the override config file
-        """
-        context = Path(context)
-        config_file = context / "config.toml"
-        if not config_file.exists():
-            print(
-                f"[bright_red bold]ERROR:[/bold] No config file found at {config_file}. "
-                f"A `config.toml` file is required in the context root."
-            )
-            raise BakeryConfigError(f"No config file found at {config_file}")
-        override_config_file = None
-        if not skip_override:
-            override_config_file = context / "config.override.toml"
-        return cls(config_file, override_config_file)
+    def merge(self, c: "Config"):
+        if len(c.registry) > 0:
+            self.registry = c.registry
+        if c.repository.authors:
+            self.repository.authors = c.repository.authors
+        if c.repository.url:
+            self.repository.url = c.repository.url
+        if c.repository.vendor:
+            self.repository.vendor = c.repository.vendor
+        if c.repository.maintainer:
+            self.repository.maintainer = c.repository.maintainer
