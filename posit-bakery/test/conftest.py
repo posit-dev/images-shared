f2e62cadaf6d812d161799e4085b9d7577c66446
Ian Pittwood <ian.pittwood@rstudio.com>
2024-11-14 10:18:20 -0700

    Markup tests and tested content with docs

--- posit-bakery/test/conftest.py
+++ posit-bakery/test/conftest.py
@@ -6,7 +6,11 @@ import pytest
 
 TEST_DIRECTORY = Path(os.path.dirname(os.path.realpath(__file__)))
 
-TOML_BASIC = """name = "basic"
+
+@pytest.fixture
+def toml_basic_str():
+    """Return a basic TOML document as a string"""
+    return """name = "basic"
 
 [section]
 key = "value"
@@ -14,12 +18,8 @@ key = "value"
 
 
 @pytest.fixture
-def toml_basic_str():
-    return TOML_BASIC
-
-
-@pytest.fixture
 def toml_basic_file(tmp_path, toml_basic_str):
+    """Return a basic TOML document as a filepath"""
     filepath = tmp_path / "basic.toml"
     with open(filepath, "w") as f:
         f.write(toml_basic_str)
@@ -28,14 +28,24 @@ def toml_basic_file(tmp_path, toml_basic_str):
 
 @pytest.fixture(scope="session")
 def test_resource_path():
+    """Return the path to the test resources directory"""
     return TEST_DIRECTORY / "resources"
 
 
 @pytest.fixture(scope="session")
 def test_suite_basic_context(test_resource_path):
+    """Return the path to the basic test suite context"""
     return test_resource_path / "basic"
 
 
 @pytest.fixture(scope="session")
 def test_suite_basic_config_file(test_suite_basic_context):
+    """Return the path to the basic test suite config.toml file"""
     return test_suite_basic_context / "config.toml"
+
+
+@pytest.fixture
+def test_suite_basic_config_obj(test_suite_basic_config_file):
+    """Return a Config object loaded from basic test suite config.toml file"""
+    from posit_bakery.models.config import Config
+    return Config.load_file(test_suite_basic_config_file)
--- posit-bakery/test/models/test_config.py
+++ posit-bakery/test/models/test_config.py
@@ -3,20 +3,25 @@ from posit_bakery.models import config
 
 class TestConfigRegistry:
     def test_create_config_registry(self):
+        """Test creating a generic ConfigRegistry object does not raise an exception"""
         config.ConfigRegistry(host="docker.io", namespace="posit")
 
     def test_create_config_registry_no_namespace(self):
+        """Test creating a generic ConfigRegistry object without a namespace does not raise an exception"""
         config.ConfigRegistry(host="docker.io")
 
     def test_base_url(self):
+        """Test the base_url property of a ConfigRegistry object"""
         c = config.ConfigRegistry(host="docker.io", namespace="posit")
         assert c.base_url == "docker.io/posit"
 
     def test_base_url_no_namespace(self):
+        """Test the base_url property of a ConfigRegistry object without a namespace"""
         c = config.ConfigRegistry(host="docker.io")
         assert c.base_url == "docker.io"
 
     def test_hash(self):
+        """Test the hash method of a ConfigRegistry object"""
         c1 = config.ConfigRegistry(host="docker.io", namespace="posit")
         c2 = config.ConfigRegistry(host="docker.io", namespace="posit")
         c3 = config.ConfigRegistry(host="ghcr.io", namespace="posit")
@@ -26,6 +31,7 @@ class TestConfigRegistry:
 
 class TestConfigRepository:
     def test_create_config_repository(self):
+        """Test creating a generic ConfigRepository object does not raise an exception"""
         config.ConfigRepository(
             authors=["author1", "author2"],
             url="github.com/rstudio/example",
@@ -34,11 +40,16 @@ class TestConfigRepository:
         )
 
     def test_create_config_repository_empty(self):
+        """Test creating a generic ConfigRepository object with no arguments does not raise an exception
+
+        Repository information is currently not expected to be required since it is used as labeling
+        """
         config.ConfigRepository()
 
 
 class TestConfig:
     def test_create_config(self, test_suite_basic_context, test_suite_basic_config_file):
+        """Test creating a generic Config object does not raise an exception and test data appears as expected"""
         c = config.Config(
             filepath=test_suite_basic_config_file,
             context=test_suite_basic_context,
@@ -59,6 +70,7 @@ class TestConfig:
         assert c.get_commit_sha() == ""
 
     def test_load_file(self, test_suite_basic_config_file):
+        """Test that the load_file method returns a Config object with expected data"""
         c = config.Config.load_file(test_suite_basic_config_file)
         assert c.authors == {"Author 1 <author1@posit.co>", "Author 2 <author2@posit.co>"}
         assert c.repository_url == "github.com/rstudio/posit-images-shared"
@@ -67,15 +79,17 @@ class TestConfig:
         assert len(c.registry) == 2
         assert c.registry_urls == ["docker.io/posit", "ghcr.io/posit-dev"]
 
-    def test_merge(self, test_suite_basic_context, test_suite_basic_config_file):
-        c = config.Config.load_file(test_suite_basic_config_file)
-        assert c.authors == {"Author 1 <author1@posit.co>", "Author 2 <author2@posit.co>"}
-        assert c.repository_url == "github.com/rstudio/posit-images-shared"
-        assert c.vendor == "Posit Software, PBC"
-        assert c.maintainer == "docker@posit.co"
-        assert len(c.registry) == 2
-        assert c.registry_urls == ["docker.io/posit", "ghcr.io/posit-dev"]
+    def test_merge(self, test_suite_basic_context, test_suite_basic_config_file, test_suite_basic_config_obj):
+        """Test that the merge method updates the Config object with the provided Config object"""
+        # Test existing values
+        assert test_suite_basic_config_obj.authors == {"Author 1 <author1@posit.co>", "Author 2 <author2@posit.co>"}
+        assert test_suite_basic_config_obj.repository_url == "github.com/rstudio/posit-images-shared"
+        assert test_suite_basic_config_obj.vendor == "Posit Software, PBC"
+        assert test_suite_basic_config_obj.maintainer == "docker@posit.co"
+        assert len(test_suite_basic_config_obj.registry) == 2
+        assert test_suite_basic_config_obj.registry_urls == ["docker.io/posit", "ghcr.io/posit-dev"]
 
+        # Test overriding values without modifying registry
         c_override = config.Config(
             filepath=test_suite_basic_config_file,
             context=test_suite_basic_context,
@@ -88,15 +102,16 @@ class TestConfig:
                 maintainer="images@example.com",
             )
         )
-        c.merge(c_override)
-        assert c.authors == {"Author 3 <author3@posit.co", "Author 4 <author4@posit.co>"}
-        assert c.repository_url == "github.com/rstudio/example"
-        assert c.vendor == "Example Company"
-        assert c.maintainer == "images@example.com"
-        assert len(c.registry) == 2
-        assert c.registry_urls == ["docker.io/posit", "ghcr.io/posit-dev"]
+        test_suite_basic_config_obj.merge(c_override)
+        assert test_suite_basic_config_obj.authors == {"Author 3 <author3@posit.co", "Author 4 <author4@posit.co>"}
+        assert test_suite_basic_config_obj.repository_url == "github.com/rstudio/example"
+        assert test_suite_basic_config_obj.vendor == "Example Company"
+        assert test_suite_basic_config_obj.maintainer == "images@example.com"
+        assert len(test_suite_basic_config_obj.registry) == 2
+        assert test_suite_basic_config_obj.registry_urls == ["docker.io/posit", "ghcr.io/posit-dev"]
 
+        # Test overriding registry
         c_override.registry = [config.ConfigRegistry(host="docker.io", namespace="example")]
-        c.merge(c_override)
-        assert len(c.registry) == 1
-        assert c.registry_urls == ["docker.io/example"]
+        test_suite_basic_config_obj.merge(c_override)
+        assert len(test_suite_basic_config_obj.registry) == 1
+        assert test_suite_basic_config_obj.registry_urls == ["docker.io/example"]
