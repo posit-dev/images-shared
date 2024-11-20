65912107328e5ae942e28ba418aeb733dd31863b
Ian Pittwood <ian.pittwood@rstudio.com>
2024-11-20 13:17:58 -0700

    Remove DirIsEmpty (unused)

--- src/pti/system/file.go
+++ src/pti/system/file.go
@@ -80,19 +80,3 @@ func IsPathExist(path string) (bool, error) {
 	}
 	return true, nil
 }
-
-// TODO: Remove me
-func DirIsEmpty(path string) (bool, error) {
-	f, err := os.Open(path)
-	if err != nil {
-		return false, err
-	}
-	defer f.Close()
-
-	_, err = f.Readdirnames(1)
-
-	if err == io.EOF {
-		return true, nil
-	}
-	return false, err
-}
