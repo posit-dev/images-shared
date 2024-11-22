4ff0464c7c6fedd8149e012c5b44b9520ba1181d
Benjamin R. J. Schwedler <ben@posit.co>
2024-11-22 12:22:08 -0600

    Use RichHandler for writing logs
    Also add a ruff linting check for any `print()` statements

--- posit-bakery/posit_bakery/main.py
+++ posit-bakery/posit_bakery/main.py
@@ -1,13 +1,24 @@
 import json
+import logging
 import os
 from pathlib import Path
 from typing import Annotated, List
 
-from rich import print, print_json
+from rich import print_json
+from rich.logging import RichHandler
 import typer
 
 from posit_bakery.models.project import Project
-from posit_bakery.error import BakeryGossError, BakeryBuildError
+from posit_bakery.error import (
+    BakeryBuildError,
+    BakeryConfigError,
+    BakeryGossError,
+)
+
+
+logging.basicConfig(level="NOTSET", format="%(message)s", datefmt="[%X]", handlers=[RichHandler(rich_tracebacks=True)])
+log = logging.getLogger("rich")
+
 
 app = typer.Typer()
 
@@ -42,9 +53,13 @@ def new(
     """
     project = Project.load(context)
     # TODO: This will fail on projects with no config.toml, we should build in a full "new project" expectation in that case
-    project.new_image(image_name, image_base)
+    try:
+        project.new_image(image_name, image_base)
+    except BakeryConfigError as e:
+        log.error(f"{e}")
+        raise typer.Exit(code=1)
 
-    print(f"[green bold]Successfully generated {image_name}[/green bold] ✅")
+    log.info(f"Successfully generated {image_name} ✅")
 
 
 @app.command()
@@ -83,13 +98,13 @@ def render(
         for v in value:
             sp = v.split("=")
             if len(sp) != 2:
-                print(f"[bright_red bold]ERROR:[/bold] Expected key=value pair, got [bold]'{v}'")
+                log.error(f"[bright_red bold]ERROR:[/bold] Expected key=value pair, got [bold]'{v}'")
                 raise typer.Exit(code=1)
             value_map[sp[0]] = sp[1]
 
     project.new_image_version(image_name, image_version, value_map, not skip_mark_latest)
 
-    print(f"[green bold]Successfully rendered {image_name}/{image_version}[/green bold] ✅")
+    log.info(f"Successfully rendered {image_name}/{image_version} ✅")
 
 
 @app.command()
@@ -151,7 +166,7 @@ def build(
     try:
         project.build(load, push, image_name, image_version, image_type, option)
     except BakeryBuildError as e:
-        print(f"[bright_red][bold]ERROR:[/bold] Build failed with exit code {e.exit_code}")
+        log.error(f"Build failed with exit code {e.exit_code}")
 
 
 @app.command()
@@ -183,4 +198,4 @@ def dgoss(
     try:
         project.dgoss(image_name, image_version, option)
     except BakeryGossError as e:
-        print(f"[bright_red][bold]ERROR:[/bold] dgoss tests failed with exit code {e.exit_code}")
+        log.error(f"dgoss tests failed with exit code {e.exit_code}")
