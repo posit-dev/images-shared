edabd2f881914fd2dfc4d12efb86ebbc2fbc541b
Ian Pittwood <ian.pittwood@rstudio.com>
2024-12-20 12:27:21 -0700

    Wrap Bootstrap with update/clean
    Add epel-release package to RHEL-based system init

--- src/pti/tools/container/bootstrap.go
+++ src/pti/tools/container/bootstrap.go
@@ -2,20 +2,35 @@ package container
 
 import (
 	"fmt"
+	"log/slog"
 	"pti/system"
 	"pti/system/syspkg"
 )
 
 func Bootstrap(l *system.LocalSystem) error {
+	err := l.PackageManager.Update()
+	defer l.PackageManager.Clean()
+	if err != nil {
+		return fmt.Errorf("failed to update package manager: %w", err)
+	}
+
+	slog.Info("Installing and configuring ca-certificates")
 	packages := &syspkg.PackageList{Packages: []string{"ca-certificates"}}
 
 	if err := l.PackageManager.Install(packages); err != nil {
 		return fmt.Errorf("failed to install ca-certificates: %w", err)
 	}
-	defer l.PackageManager.Clean()
 
 	if err := l.UpdateCACertificates(); err != nil {
 		return fmt.Errorf("failed to update CA certificates: %w", err)
+	}
+
+	if l.PackageManager.GetBin() == "dnf" {
+		slog.Info("Installing EPEL repository")
+		err = l.PackageManager.Install(&syspkg.PackageList{Packages: []string{"epel-release"}})
+		if err != nil {
+			return fmt.Errorf("failed to install epel-release: %w", err)
+		}
 	}
 
 	return nil
