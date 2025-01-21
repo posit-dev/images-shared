2cf7a13de857dbd53eea28d192a25b07b07bce79
Ian Pittwood <ian.pittwood@rstudio.com>
2025-01-21 11:33:47 -0700

    Allow multiple options for container monitor lifecycle, environment, and business criticality

--- posit-bakery/posit_bakery/models/manifest/snyk.py
+++ posit-bakery/posit_bakery/models/manifest/snyk.py
@@ -1,6 +1,6 @@
 import logging
 import re
-from typing import Annotated, Dict, Self
+from typing import Annotated, Dict, Self, List, Union
 
 from pydantic import BaseModel, Field, model_validator
 from pydantic.functional_validators import field_validator
@@ -27,6 +27,39 @@ DEFAULT_SNYK_SBOM_FORMAT = "cyclonedx1.5+json"
 log = logging.getLogger("rich")
 
 
+def validate_list_of_strings_or_string(value: Union[List[str], str], regex_pat: re.Pattern, message: str) -> Union[List[str], str]:
+    if isinstance(value, str):
+        # If value is a string containing commas, parse and validate it as a list of string input values
+        if "," in value:
+            split_values = value.split(",")
+            return_values = []
+            for split_value in split_values:
+                if not regex_pat.match(split_value):
+                    log.warning(message % split_value)
+                else:
+                    return_values.append(split_value)
+            if len(return_values) > 0:
+                return return_values
+        # If value is a single string, validate it as a single string input value
+        else:
+            if regex_pat.match(value):
+                return value
+            else:
+                log.warning(message % value)
+    # If value is a list of strings, validate each string in the list and return the list of valid strings
+    if isinstance(value, list):
+        return_values = []
+        for val in value:
+            if not regex_pat.match(val):
+                log.warning(message % val)
+            else:
+                return_values.append(val)
+        if len(return_values) > 0:
+            return return_values
+
+    raise PydanticUseDefault()
+
+
 class ManifestSnykTestOutput(BaseModel):
     format: Annotated[str, Field(pattern=REGEX_SNYK_TEST_OUTPUT_FORMAT)] = DEFAULT_SNYK_TEST_OUTPUT_FORMAT
     json_file: bool = False
@@ -47,7 +80,9 @@ class ManifestSnykTestOutput(BaseModel):
 
 
 class ManifestSnykTest(BaseModel):
-    severity_threshold: Annotated[str, Field(pattern=REGEX_SNYK_TEST_SEVERITY_THRESHOLD)] = DEFAULT_SNYK_TEST_SEVERITY_THRESHOLD
+    severity_threshold: Annotated[
+        str, Field(pattern=REGEX_SNYK_TEST_SEVERITY_THRESHOLD)
+    ] = DEFAULT_SNYK_TEST_SEVERITY_THRESHOLD
     include_app_vulns: bool = True
     include_base_image_vulns: bool = False
     output: Annotated[ManifestSnykTestOutput, Field(default_factory=ManifestSnykTestOutput)]
@@ -69,47 +104,62 @@ class ManifestSnykTest(BaseModel):
 class ManifestSnykMonitor(BaseModel):
     include_app_vulns: bool = True
     output_json: bool = False
-    environment: Annotated[str | None, Field(pattern=REGEX_SNYK_MONITOR_PROJECT_ENVIRONMENT)] = None
-    lifecycle: Annotated[str | None, Field(pattern=REGEX_SNYK_MONITOR_PROJECT_LIFECYCLE)] = None
-    business_criticality: Annotated[str | None, Field(pattern=REGEX_SNYK_MONITOR_PROJECT_BUSINESS_CRITICALITY)] = None
+    environment: Annotated[List[str] | str | None, Field(pattern=REGEX_SNYK_MONITOR_PROJECT_ENVIRONMENT)] = None
+    lifecycle: Annotated[List[str] | str | None, Field(pattern=REGEX_SNYK_MONITOR_PROJECT_LIFECYCLE)] = None
+    business_criticality: Annotated[
+        List[str] | str | None, Field(pattern=REGEX_SNYK_MONITOR_PROJECT_BUSINESS_CRITICALITY)
+    ] = None
     tags: Dict[str, str] | None = None
 
-    @field_validator("environment", mode="wrap")
+    @field_validator("environment", mode="before")
     @classmethod
-    def validate_environment_to_warning(cls, value, handler):
-        try:
-            return handler(value)
-        except ValueError:
+    def validate_environment_to_warning(cls, value: Union[List[str], str, None]) -> Union[List[str], str, None]:
+        warn_message = (
+            "Invalid value for snyk.monitor.environment, expected '%s' to match regex "
+            f"pattern '{REGEX_SNYK_MONITOR_PROJECT_ENVIRONMENT.pattern}'. Environment will not be "
+            "passed to Snyk."
+        )
+
+        # If value is an empty string, use default value
+        if value == "":
+            log.warning("Invalid value for snyk.monitor.environment, expected a non-empty string or list of strings.")
+            raise PydanticUseDefault()
+
+        return validate_list_of_strings_or_string(value, REGEX_SNYK_MONITOR_PROJECT_ENVIRONMENT, warn_message)
+
+    @field_validator("lifecycle", mode="before")
+    @classmethod
+    def validate_lifecycle_to_warning(cls, value: Union[List[str], str, None]) -> Union[List[str], str, None]:
+        warn_message = (
+            "Invalid value for snyk.monitor.lifecycle, expected '%s' to match regex pattern "
+            f"'{REGEX_SNYK_MONITOR_PROJECT_LIFECYCLE.pattern}'. Lifecycle will not be passed to Snyk."
+        )
+
+        # If value is an empty string, use default value
+        if value == "":
+            log.warning("Invalid value for snyk.monitor.lifecycle, expected a non-empty string or list of strings.")
+            raise PydanticUseDefault()
+
+        return validate_list_of_strings_or_string(value, REGEX_SNYK_MONITOR_PROJECT_LIFECYCLE, warn_message)
+
+    @field_validator("business_criticality", mode="before")
+    @classmethod
+    def validate_business_criticality_to_warning(
+            cls, value: Union[List[str], str, None]
+    ) -> Union[List[str], str, None]:
+        warn_message = (
+            "Invalid value for snyk.monitor.business_criticality, expected '%s' to match regex pattern "
+            f"'{REGEX_SNYK_MONITOR_PROJECT_BUSINESS_CRITICALITY.pattern}'. Business criticality will not be "
+            "passed to Snyk."
+        )
+
+        if value == "":
             log.warning(
-                f"Invalid value for snyk.monitor.environment, expected '{value}' to match regex pattern "
-                f"'{REGEX_SNYK_MONITOR_PROJECT_ENVIRONMENT.pattern}'. Environment will not be passed to Snyk."
+                "Invalid value for snyk.monitor.business_criticality, expected a non-empty string or list of strings."
             )
             raise PydanticUseDefault()
 
-    @field_validator("lifecycle", mode="wrap")
-    @classmethod
-    def validate_lifecycle_to_warning(cls, value, handler):
-        try:
-            return handler(value)
-        except ValueError:
-            log.warning(
-                f"Invalid value for snyk.monitor.lifecycle, expected '{value}' to match regex pattern "
-                f"'{REGEX_SNYK_MONITOR_PROJECT_LIFECYCLE.pattern}'. Lifecycle will not be passed to Snyk."
-            )
-            raise PydanticUseDefault()
-
-    @field_validator("business_criticality", mode="wrap")
-    @classmethod
-    def validate_business_criticality_to_warning(cls, value, handler):
-        try:
-            return handler(value)
-        except ValueError:
-            log.warning(
-                f"Invalid value for snyk.monitor.business_criticality, expected '{value}' to match regex pattern "
-                f"'{REGEX_SNYK_MONITOR_PROJECT_BUSINESS_CRITICALITY.pattern}'. Business criticality will not be "
-                f"passed to Snyk."
-            )
-            raise PydanticUseDefault()
+        return validate_list_of_strings_or_string(value, REGEX_SNYK_MONITOR_PROJECT_BUSINESS_CRITICALITY, warn_message)
 
     @model_validator(mode="after")
     def validate_tags(self) -> Self:
