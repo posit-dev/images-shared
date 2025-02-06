5e915b13538e3111b74357b2d317289eaf7e6d08
Ian Pittwood <ian.pittwood@rstudio.com>
2025-02-06 14:57:38 -0700

    Add run commands

--- posit-bakery/posit_bakery/cli/main.py
+++ posit-bakery/posit_bakery/cli/main.py
@@ -1,6 +1,6 @@
 import typer
 
-from posit_bakery.cli import create, common
+from posit_bakery.cli import create, common, run
 
 app = typer.Typer(no_args_is_help=True, callback=common.__global_flags)
 
@@ -8,3 +8,5 @@ app = typer.Typer(no_args_is_help=True, callback=common.__global_flags)
 app.add_typer(create.app, name="create", help="Create new projects, images, and versions (aliases: c, new)")
 app.add_typer(create.app, name="c", hidden=True)
 app.add_typer(create.app, name="new", hidden=True)
+app.add_typer(run.app, name="run", help="Run commands against images (aliases: r)")
+app.add_typer(run.app, name="r", hidden=True)
