447bd61bee0afe2dda79b240f8905daa148e7cd2
Ian Pittwood <ian.pittwood@rstudio.com>
2025-02-24 15:15:46 -0700

    Fix warning for unclosed file handles

--- posit-bakery/posit_bakery/templating/__init__.py
+++ posit-bakery/posit_bakery/templating/__init__.py
@@ -3,6 +3,6 @@ from pathlib import Path
 
 TEMPLATE_DIR: Path = Path(os.path.dirname(os.path.realpath(__file__))) / "templates"
 
-TPL_CONFIG_TOML = open(TEMPLATE_DIR / "config.toml.jinja2").read()
-TPL_MANIFEST_TOML = open(TEMPLATE_DIR / "manifest.toml.jinja2").read()
-TPL_CONTAINERFILE = open(TEMPLATE_DIR / "containerfile.jinja2").read()
+TPL_CONFIG_TOML = (TEMPLATE_DIR / "config.toml.jinja2").read_text()
+TPL_MANIFEST_TOML = (TEMPLATE_DIR / "manifest.toml.jinja2").read_text()
+TPL_CONTAINERFILE = (TEMPLATE_DIR / "containerfile.jinja2").read_text()
