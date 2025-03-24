f382081583c41638001b17f88fc576291bb79222
Ian Pittwood <ian.pittwood@rstudio.com>
2025-03-24 11:11:33 -0600

    Add missing f-string

--- posit-bakery/posit_bakery/models/project/project.py
+++ posit-bakery/posit_bakery/models/project/project.py
@@ -340,7 +340,7 @@ class Project(BaseModel):
                 cmd.extend(["-e", f"IMAGE_TYPE={variant.target}"])
 
             if variant.goss.entrypoint is not None:
-                cmd.extend(["--entrypoint", "'variant.goss.entrypoint'"])
+                cmd.extend(["--entrypoint", f"'{variant.goss.entrypoint}'"])
 
             # Add user runtime options if provided
             if runtime_options:
