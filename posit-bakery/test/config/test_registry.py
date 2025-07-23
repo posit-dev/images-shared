2522f55b0a4d135cec241b6c8863ac77fb62f753
Ian Pittwood <ian.pittwood@rstudio.com>
2025-07-23 14:17:51 -0600

    Transfer registry tests

---
+++ posit-bakery/test/config/test_registry.py
@@ -0,0 +1,33 @@
+import pytest
+
+from posit_bakery.config.registry import Registry
+
+
+@pytest.mark.config
+@pytest.mark.schema
+class TestConfigRegistry:
+    def test_create_config_registry(self):
+        """Test creating a generic ConfigRegistry object does not raise an exception"""
+        Registry(host="docker.io", namespace="posit")
+
+    def test_create_config_registry_no_namespace(self):
+        """Test creating a generic ConfigRegistry object without a namespace does not raise an exception"""
+        Registry(host="docker.io")
+
+    def test_base_url(self):
+        """Test the base_url property of a ConfigRegistry object"""
+        c = Registry(host="docker.io", namespace="posit")
+        assert c.base_url == "docker.io/posit"
+
+    def test_base_url_no_namespace(self):
+        """Test the base_url property of a ConfigRegistry object without a namespace"""
+        c = Registry(host="docker.io")
+        assert c.base_url == "docker.io"
+
+    def test_hash(self):
+        """Test the hash method of a ConfigRegistry object"""
+        c1 = Registry(host="docker.io", namespace="posit")
+        c2 = Registry(host="docker.io", namespace="posit")
+        c3 = Registry(host="ghcr.io", namespace="posit")
+        assert hash(c1) == hash(c2)
+        assert hash(c1) != hash(c3)
