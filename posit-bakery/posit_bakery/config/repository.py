493be9de2c64ddf2ae817e77c7196a049aec4918
Ian Pittwood <ian.pittwood@rstudio.com>
2025-07-23 14:20:40 -0600

    Wrap NameEmail to make it hashable for sets
    Add pre-validators to properly interpret a dictionary with "name" and "email" values or a string to defer validation to NameEmail

--- posit-bakery/posit_bakery/config/repository.py
+++ posit-bakery/posit_bakery/config/repository.py
@@ -1,21 +1,66 @@
 import logging
-from typing import Annotated
+from typing import Annotated, Any
 
-from pydantic import BaseModel, HttpUrl, NameEmail, Field, field_validator
-
+from pydantic import BaseModel, HttpUrl, NameEmail, Field, field_validator, AnyUrl
 
 log = logging.getLogger(__name__)
+
+
+class HashableNameEmail(NameEmail):
+    """A hashable version of NameEmail for use in sets"""
+
+    def __hash__(self) -> int:
+        """Unique hash for a NameEmail object"""
+        return hash(str(self))
+
+
+def parse_name_email_from_dict(name_email_dict: dict) -> HashableNameEmail:
+    """Parse a dictionary into a NameEmail object"""
+    if "email" not in name_email_dict:
+        raise ValueError(f"Invalid input '{name_email_dict}': 'email' must be provided")
+    return HashableNameEmail(**name_email_dict)
 
 
 class Repository(BaseModel):
     url: HttpUrl
     vendor: Annotated[str, Field(default="Posit Software, PBC")]
-    maintainer: Annotated[NameEmail, Field(default=NameEmail(name="Posit Docker Team", email="docker@posit.co"))]
-    authors: Annotated[list[NameEmail], Field(default_factory=list)]
+    maintainer: Annotated[
+        HashableNameEmail, Field(default=NameEmail(name="Posit Docker Team", email="docker@posit.co"))
+    ]
+    authors: Annotated[list[HashableNameEmail], Field(default_factory=list)]
+
+    @field_validator("authors", mode="before")
+    @classmethod
+    def parse_authors(cls, value: list[Any]) -> list[HashableNameEmail | str]:
+        """Parse the authors field into a list of NameEmail objects from dicts or as strings for later validation"""
+        parsed_authors: list[HashableNameEmail | str] = []
+        for v in value:
+            if isinstance(v, dict):
+                parsed_authors.append(parse_name_email_from_dict(v))
+            elif isinstance(v, str):
+                parsed_authors.append(v)
+            else:
+                raise ValueError(
+                    f"Invalid input for author '{v}': must be a string or dict with 'name' and 'email' keys"
+                )
+        return parsed_authors
+
+    @field_validator("maintainer", mode="before")
+    @classmethod
+    def parse_maintainer(cls, value: Any) -> HashableNameEmail | str:
+        """Parse the maintainer field into a NameEmail object or return as is if already a str for later validation"""
+        if isinstance(value, dict):
+            return parse_name_email_from_dict(value)
+        elif isinstance(value, str):
+            return value
+        else:
+            raise ValueError(
+                f"Invalid input for maintainer '{value}': must be a string or dict with 'name' and 'email' keys"
+            )
 
     @field_validator("authors", mode="after")
     @classmethod
-    def validate_authors(
+    def deduplicate_authors(
         cls,
         authors: list[NameEmail],
     ) -> list[NameEmail]:
