054d68efaedf32ee7a4cf74e6606857835e3e984
Ian Pittwood <ian.pittwood@rstudio.com>
2025-07-23 13:52:03 -0600

    Add "base path" as a hidden model field for configs to track the project directory

--- posit-bakery/posit_bakery/config/config.py
+++ posit-bakery/posit_bakery/config/config.py
@@ -16,10 +16,12 @@ class BakeryConfig:
     def __init__(self, config_file: str | Path | os.PathLike):
         self.yaml = YAML()
         self.config_file = Path(config_file)
-        self.config = BakeryConfigDocument(**self.yaml.load(self.config_file))
+        self.base_path = self.config_file.parent
+        self.config = BakeryConfigDocument(base_path=self.base_path, **self.yaml.load(self.config_file))
 
 
 class BakeryConfigDocument(BaseModel):
+    base_path: Annotated[Path, Field(exclude=True)]
     repository: Repository
     registries: Annotated[list[Registry], Field(default_factory=list)]
     images: Annotated[list[Image], Field(default_factory=list)]
