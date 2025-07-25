181b8c20c0a570d61a3d41a44dcf4ab5323cc3d9
Ian Pittwood <ian.pittwood@rstudio.com>
2025-07-25 13:06:53 -0600

    Add test for hash function

--- posit-bakery/test/config/test_tag.py
+++ posit-bakery/test/config/test_tag.py
@@ -24,6 +24,12 @@ class TestTagPattern:
         rendered_tags = tag_pattern.render(Version="1.0", OS="ubuntu2204", Variant="min")
         assert rendered_tags == ["1.0-ubuntu2204-min", "min"]
 
+    def test_hash_tag_pattern(self):
+        tag_pattern1 = TagPattern(patterns=["{{ Version }}-{{ OS }}-{{ Variant }}"], only=[TagPatternFilter.ALL])
+        tag_pattern2 = TagPattern(patterns=["{{ Version }}-{{ OS }}-{{ Variant }}"], only=[TagPatternFilter.ALL])
+        assert tag_pattern1 == tag_pattern2
+        assert hash(tag_pattern1) == hash(tag_pattern2)
+
 
 def test_default_tag_patterns():
     """Test that the default tag patterns are created correctly."""
