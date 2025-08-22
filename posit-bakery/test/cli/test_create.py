650bc24ebcca6202dcb860929e200b5a1895213b
Ian Pittwood <ian.pittwood@rstudio.com>
2025-08-22 14:03:56 -0600

    Add tests for existing image/version scenarios and subpathing

--- posit-bakery/test/cli/test_create.py
+++ posit-bakery/test/cli/test_create.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from pytest_bdd import scenarios, then, parsers
 
 from posit_bakery.config import BakeryConfig
@@ -16,21 +18,27 @@ def check_bakery_file(bakery_command):
 
 
 @then(parsers.parse('the image "{image_name}" exists'), target_fixture="cli_test_image_name")
-def check_image(cli_test_tmpcontext, image_name) -> str:
+@then(parsers.parse('the image "{image_name}" exists in the "{subpath}" subpath'), target_fixture="cli_test_image_name")
+def check_image(cli_test_tmpcontext, image_name, subpath=None) -> str:
     image_dir = cli_test_tmpcontext / image_name
+    if subpath:
+        image_dir = cli_test_tmpcontext / Path(subpath)
     assert image_dir.is_dir()
-    tpl_dir = cli_test_tmpcontext / image_name / "template"
+    tpl_dir = image_dir / "template"
     assert tpl_dir.is_dir()
 
     return image_name
 
 
 @then(parsers.parse('the version "{version}" exists'), target_fixture="cli_test_version")
-def check_version(cli_test_tmpcontext, version, cli_test_image_name) -> str:
+@then(parsers.parse('the version "{version}" exists in the "{subpath}" subpath'), target_fixture="cli_test_version")
+def check_version(cli_test_tmpcontext, version, cli_test_image_name, subpath=None) -> str:
     config = BakeryConfig.from_context(cli_test_tmpcontext)
     image = config.model.get_image(cli_test_image_name)
 
     version_dir = image.path / version
+    if subpath:
+        version_dir = image.path / Path(subpath)
     assert version_dir.is_dir()
 
     assert image.get_version(version) is not None
@@ -41,8 +49,11 @@ def check_version(cli_test_tmpcontext, version, cli_test_image_name) -> str:
 
 
 @then(parsers.parse("the default templates exist"))
-def check_default_templates(cli_test_tmpcontext, cli_test_image_name) -> None:
+@then(parsers.parse('the default templates exist in the "{subpath}" subpath'))
+def check_default_templates(cli_test_tmpcontext, cli_test_image_name, subpath=None) -> None:
     image_dir = cli_test_tmpcontext / cli_test_image_name
+    if subpath:
+        image_dir = cli_test_tmpcontext / Path(subpath)
     template_dir = image_dir / "template"
     assert template_dir.is_dir()
 
@@ -59,16 +70,22 @@ def check_default_templates(cli_test_tmpcontext, cli_test_image_name) -> None:
 
 
 @then(parsers.parse('the default base image is "{base_image}"'))
-def check_base_image(cli_test_tmpcontext, base_image, cli_test_image_name) -> None:
+@then(parsers.parse('the default base image is "{base_image}" in the "{subpath}" subpath'))
+def check_base_image(cli_test_tmpcontext, base_image, cli_test_image_name, subpath=None) -> None:
     files = list((cli_test_tmpcontext / cli_test_image_name / "template").rglob("Containerfile*jinja2"))
+    if subpath:
+        files = list((cli_test_tmpcontext / Path(subpath) / "template").rglob("Containerfile*jinja2"))
 
     assert len(files) == 1
     assert f"FROM {base_image}" in files[0].read_text()
 
 
 @then("the default rendered templates exist")
-def check_rendered_templates(cli_test_tmpcontext, cli_test_image_name, cli_test_version) -> None:
+@then(parsers.parse('the default rendered templates exist in the "{subpath}" subpath'))
+def check_rendered_templates(cli_test_tmpcontext, cli_test_image_name, cli_test_version, subpath=None) -> None:
     version_dir = cli_test_tmpcontext / cli_test_image_name / cli_test_version
+    if subpath:
+        version_dir = cli_test_tmpcontext / cli_test_image_name / Path(subpath)
     _os = "ubuntu2204"
 
     min = version_dir / f"Containerfile.{_os}.min"
--- posit-bakery/test/features/cli/create/version.feature
+++ posit-bakery/test/features/cli/create/version.feature
@@ -1,7 +1,7 @@
 @functional
 Feature: create version
 
-    Scenario: bakery create version
+    Scenario: Creating a new version
         Given I call bakery create version
         * in a temp basic context
         * with the arguments:
@@ -15,3 +15,51 @@ Feature: create version
         * the stderr output includes:
             | Successfully created version |
             | 'test-image/1.1.0' |
+
+    Scenario: Creating a new version with a custom subpath
+        Given I call bakery create version
+        * in a temp basic context
+        * with the arguments:
+            | test-image |
+            | 1.1.0 |
+            | --subpath |
+            | 1.1 |
+        When I execute the command
+        Then The command succeeds
+        * the image "test-image" exists
+        * the version "1.1.0" exists in the "1.1" subpath
+        * the default rendered templates exist in the "1.1" subpath
+        * the stderr output includes:
+            | Successfully created version |
+            | 'test-image/1.1.0' |
+
+    Scenario: Creating a duplicate version fails without --force
+        Given I call bakery create version
+        * in a temp basic context
+        * with the arguments:
+            | test-image |
+            | 1.0.0 |
+        When I execute the command
+        Then The command exits with code 1
+        * the image "test-image" exists
+        * the version "1.0.0" exists
+        * the default rendered templates exist
+        * the stderr output includes:
+            | Failed to create version |
+            | 'test-image/1.0.0' |
+
+    Scenario: Creating a duplicate version works with --force
+        Given I call bakery create version
+        * in a temp basic context
+        * with the arguments:
+            | test-image |
+            | 1.0.0 |
+            | --force |
+        When I execute the command
+        Then The command succeeds
+        * the image "test-image" exists
+        * the version "1.0.0" exists
+        * the default rendered templates exist
+        * the stderr output includes:
+            | Successfully created version |
+            | 'test-image/1.0.0' |
