630967ee0cd72fb73800c374bdf82de0828e4f68
Ian Pittwood <ian.pittwood@rstudio.com>
2025-09-23 13:50:58 -0400

    Trying out a new output format for suspected dgoss runtime errors

--- posit-bakery/posit_bakery/error.py
+++ posit-bakery/posit_bakery/error.py
@@ -1,4 +1,5 @@
 import os
+import textwrap
 from typing import Union, List
 
 
@@ -88,6 +89,38 @@ class BakeryToolRuntimeError(BakeryToolError):
     def __str__(self) -> str:
         s = f"{self.message}'\n"
         s += f"  - Exit code: {self.exit_code}\n"
+        s += f"  - Command executed: {' '.join(self.cmd)}\n"
+        if self.metadata:
+            s += "  - Metadata:\n"
+            for key, value in self.metadata.items():
+                s += f"    - {key}: {value}\n"
+        return s
+
+
+class BakeryDGossError(BakeryToolRuntimeError):
+    def __init__(
+        self,
+        message: str = None,
+        tool_name: str = None,
+        cmd: List[str] = None,
+        stdout: str | bytes | None = None,
+        stderr: str | bytes | None = None,
+        exit_code: int = 1,
+        parse_error: Exception = None,
+        metadata: dict | None = None,
+    ) -> None:
+        super().__init__(message, tool_name)
+        self.exit_code = exit_code
+        self.cmd = cmd
+        self.stdout = stdout
+        self.stderr = stderr
+        self.parse_error = parse_error
+        self.metadata = metadata
+
+    def __str__(self) -> str:
+        s = f"{self.message}'\n"
+        s += f"  - Exit code: {self.exit_code}\n"
+        s += f"  - Command output: {textwrap.indent(self.dump_stdout(), '    ')}\n"
         s += f"  - Command executed: {' '.join(self.cmd)}\n"
         if self.metadata:
             s += "  - Metadata:\n"
