7eb1b7f73d18dd8cce7e8a52524a3b5b9aba06d6
Ian Pittwood <ian.pittwood@posit.co>
2025-09-23 08:50:33 -0600

    Small `dgoss` behavior tweaks (#225)
    * Escape whitespace and reserved characters for `dgoss` calls
    
    * Add platform flag to `dgoss` commands
    
    * Fix command expectations

--- posit-bakery/posit_bakery/image/goss/dgoss.py
+++ posit-bakery/posit_bakery/image/goss/dgoss.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import os
+import re
 import shutil
 import subprocess
 from pathlib import Path
@@ -136,11 +137,12 @@ class DGossCommand(BaseModel):
     @property
     def command(self) -> list[str]:
         """Return the full DGoss command to run."""
-        cmd = [self.dgoss_bin, self.dgoss_command]
+        cmd = [self.dgoss_bin, self.dgoss_command, "--platform", "linux/amd64"]
         for mount in self.volume_mounts:
             cmd.extend(["-v", f"{mount[0]}:{mount[1]}"])
         for env_var, value in self.image_environment.items():
-            cmd.extend(["-e", f"{env_var}={value}"])
+            env_value = re.sub(r"([\"\'\\$`!*?&#()|<>;\[\]{}\s])", r"\\\1", value)
+            cmd.extend(["-e", f"{env_var}={env_value}"])
         cmd.append("--init")
         if self.runtime_options:
             # TODO: We may want to validate this to ensure options are not duplicated.
