a8571b7162d19151cb14b49792186cbd9b53e29f
Ian Pittwood <ian.pittwood@rstudio.com>
2025-09-25 12:24:10 -0400

    Implement new messaging into error groups

--- posit-bakery/posit_bakery/error.py
+++ posit-bakery/posit_bakery/error.py
@@ -109,18 +109,21 @@ class BakeryDGossError(BakeryToolRuntimeError):
         parse_error: Exception = None,
         metadata: dict | None = None,
     ) -> None:
-        super().__init__(message, tool_name)
-        self.exit_code = exit_code
-        self.cmd = cmd
-        self.stdout = stdout
-        self.stderr = stderr
+        super().__init__(
+            message=message,
+            tool_name=tool_name,
+            cmd=cmd,
+            stdout=stdout,
+            stderr=stderr,
+            exit_code=exit_code,
+            metadata=metadata,
+        )
         self.parse_error = parse_error
-        self.metadata = metadata
 
     def __str__(self) -> str:
         s = f"{self.message}'\n"
         s += f"  - Exit code: {self.exit_code}\n"
-        s += f"  - Command output: {textwrap.indent(self.dump_stdout(), '    ')}\n"
+        s += f"  - Command output: \n{textwrap.indent(self.dump_stdout(), '      ')}\n"
         s += f"  - Command executed: {' '.join(self.cmd)}\n"
         if self.metadata:
             s += "  - Metadata:\n"
@@ -135,13 +138,7 @@ class BakeryToolRuntimeErrorGroup(ExceptionGroup):
     def __str__(self) -> str:
         s = f""
         for e in self.exceptions:
-            s += f"{e.message}\n"
-            s += f"  - Command executed: '{' '.join(e.cmd)}'\n"
-            if e.metadata:
-                s += "  - Metadata:\n"
-                for key, value in e.metadata.items():
-                    s += f"    - {key}: {value}\n"
-            s += "\n"
+            s += f"{e}\n"
         s += "\n"
         s += f"{len(self.exceptions)} command(s) returned errors\n"
 
