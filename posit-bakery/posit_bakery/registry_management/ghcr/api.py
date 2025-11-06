9994aa1296ce0ef135c83831f4660e79ab03aef6
Ian Pittwood <ian.pittwood@rstudio.com>
2025-11-06 08:18:29 -0700

    Refactor GHCR implementation as an API wrapper class

--- posit-bakery/posit_bakery/registry_management/ghcr/api.py
+++ posit-bakery/posit_bakery/registry_management/ghcr/api.py
@@ -8,64 +8,66 @@ from github import Auth, Github, GithubException
 from posit_bakery.registry_management.ghcr.models import GHCRPackageVersions, GHCRPackageVersion
 
 
-def github_client(token: str = os.getenv("GITHUB_TOKEN")) -> Github:
-    """Authenticate with Github using PyGithub and return a Github client."""
-    auth = Auth.Token(token)
-    return Github(auth=auth)
+class GHCRClient:
+    ENDPOINTS = {
+        "package": "/orgs/{organization}/packages/container/{package}",
+        "package_versions": "/orgs/{organization}/packages/container/{package}/versions",
+    }
 
+    def __init__(self, organization: str, token: str = os.getenv("GITHUB_TOKEN")):
+        self.organization = organization
+        auth = Auth.Token(token)
+        self.client = Github(auth=auth)
 
-def get_package(
-    client: Github,
-    organization: str,
-    package: str,
-) -> dict:
-    """Get details on a package."""
-    headers, response = client.requester.requestJsonAndCheck(
-        "GET",
-        f"/orgs/{organization}/packages/container/{quote(package)}",
-    )
-    response = json.loads(response)
-    return response
-
-
-def get_package_versions(
-    client: Github,
-    organization: str,
-    package: str,
-) -> GHCRPackageVersions:
-    # Check the number of versions for the package to calculate pagination cycles required.
-    response = get_package(client, organization, package)
-    version_count = response.get("versionCount", 0)
-    per_page = 100
-    page_count = math.ceil(version_count / per_page)
-
-    results = []
-    for page in range(1, page_count + 1):
-        headers, response = client.requester.requestJsonAndCheck(
+    def get_package(
+        self,
+        organization: str,
+        package: str,
+    ) -> dict:
+        """Get details on a package."""
+        headers, response = self.client.requester.requestJsonAndCheck(
             "GET",
-            f"/orgs/{organization}/packages/container/{quote(package)}/versions",
-            parameters={"per_page": 100, "page": page, "state": "active"},
+            self.ENDPOINTS["package"].format(organization=organization, package=quote(package)),
         )
         response = json.loads(response)
-        results.extend(response)
+        return response
 
-    return GHCRPackageVersions(versions=results)
+    def get_package_versions(
+        self,
+        organization: str,
+        package: str,
+    ) -> GHCRPackageVersions:
+        # Check the number of versions for the package to calculate pagination cycles required.
+        response = self.get_package(organization, package)
+        version_count = response.get("versionCount", 0)
+        per_page = 100
+        page_count = math.ceil(version_count / per_page)
 
+        results = []
+        for page in range(1, page_count + 1):
+            headers, response = self.client.requester.requestJsonAndCheck(
+                "GET",
+                self.ENDPOINTS["package_versions"].format(organization=organization, package=quote(package)),
+                parameters={"per_page": 100, "page": page, "state": "active"},
+            )
+            response = json.loads(response)
+            results.extend(response)
 
-def delete_package_version(
-    client: Github,
-    version: GHCRPackageVersion,
-):
-    target_endpoint = version.url.removeprefix("https://api.github.com")
-    client.requester.requestJsonAndCheck(
-        "DELETE",
-        target_endpoint,
-    )
+        return GHCRPackageVersions(versions=results)
 
+    def delete_package_version(
+        self,
+        version: GHCRPackageVersion,
+    ):
+        target_endpoint = version.url.removeprefix("https://api.github.com")
+        self.client.requester.requestJsonAndCheck(
+            "DELETE",
+            target_endpoint,
+        )
 
-def delete_package_versions(
-    client: Github,
-    versions: GHCRPackageVersions,
-):
-    for version in versions.versions:
-        delete_package_version(client, version)
+    def delete_package_versions(
+        self,
+        versions: GHCRPackageVersions,
+    ):
+        for version in versions.versions:
+            self.delete_package_version(version)
