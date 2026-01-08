c72eba44e700bde010859a6395c5ad2c977ca14c
Ian H. Pittwood <ian.pittwood@posit.co>
2026-01-08 08:53:55 -0700

    Test registry container service

--- posit-bakery/posit_bakery/services/registry_container.py
+++ posit-bakery/posit_bakery/services/registry_container.py
@@ -6,29 +6,41 @@ log = logging.getLogger(__name__)
 
 
 class RegistryContainer:
-    def __init__(self, image: str = "docker.io/registry:3", port: int = 5000, restart_policy: str = "always"):
+    _CONTAINER_PORT = 5000
+
+    def __init__(self, image: str = "docker.io/registry:3", port: int | None = None, restart_policy: str = "always"):
         log.debug(f"Starting registry container at port {port}...")
-        self._port = port
         self._container = python_on_whales.docker.run(
             image=image,
-            publish=[(port, 5000)],
+            publish=[(port, self._CONTAINER_PORT)] if port is not None else [(self._CONTAINER_PORT,)],
             restart=restart_policy,
+            detach=True,
         )
         log.debug("Started registry container.")
 
+    @property
     def url(self):
         """Get the URL of the registry."""
-        return f"localhost:{self._port}"
+        if not self._container.exists():
+            raise RuntimeError("Registry container does not exist.")
 
+        port_map = self._container.network_settings.ports.get(f"{self._CONTAINER_PORT}/tcp")
+        if not port_map:
+            raise RuntimeError("Registry container port is not mapped.")
+        mapped_port = port_map[0]["HostPort"]
+
+        return f"localhost:{mapped_port}"
+
+    @property
     def status(self):
         """Get the status of the registry container."""
         if not self._container.exists():
             return "not_found"
-        return self._container.status
+        return self._container.state.status
 
     def stop(self, timeout: int | None = None):
         """Stop the registry container."""
-        self._container.stop(timeout=timeout)
+        self._container.stop(time=timeout)
 
     def remove(self, force: bool = False, volumes: bool = True):
         """Remove the registry container."""
@@ -39,7 +51,7 @@ class RegistryContainer:
         self._container.kill(signal=signal)
 
     def __enter__(self):
-        return self._container
+        return self
 
     def __exit__(self, exc_type, exc_value, traceback):
         if not self._container.exists():
