2714f9fa423c02a43b320c25bb685dc98dc6897a
Benjamin R. J. Schwedler <ben@posit.co>
2026-06-04 14:38:08 -0500

    Add POSITRON product enum and daily CDN URL template
    Add ProductEnum.POSITRON and support for DAILY release
    channel. The POSITRON_DAILY_CDN_URL_TEMPLATE includes a
    {positron_cdn_arch} placeholder that will be formatted at
    fetch time by Task 3.

--- posit-bakery/posit_bakery/config/image/posit_product/const.py
+++ posit-bakery/posit_bakery/config/image/posit_product/const.py
@@ -30,6 +30,7 @@ WORKBENCH_DAILY_URL = "https://dailies.rstudio.com/rstudio/{release_branch}/inde
 PACKAGE_MANAGER_DAILY_URL = "https://cdn.posit.co/package-manager/deb/amd64/rstudio-pm-main-latest.txt"
 PACKAGE_MANAGER_PREVIEW_URL = "https://cdn.posit.co/package-manager/deb/amd64/rstudio-pm-rc-latest.txt"
 CONNECT_DAILY_URL = "https://cdn.posit.co/connect/latest-packages.json"
+POSITRON_DAILY_CDN_URL_TEMPLATE = "https://cdn.posit.co/positron/dailies/pwb/{positron_cdn_arch}/releases.json"
 DOWNLOADS_JSON_URL = "https://posit.co/wp-content/uploads/downloads.json"
 
 
@@ -38,6 +39,7 @@ class ProductEnum(str, Enum):
     PACKAGE_MANAGER = "package-manager"
     WORKBENCH = "workbench"
     WORKBENCH_SESSION = "workbench-session"
+    POSITRON = "positron"
 
 
 class ReleaseChannelEnum(str, Enum):
@@ -53,6 +55,7 @@ PRODUCT_RELEASE_CHANNEL_SUPPORT_MAP = {
     ProductEnum.CONNECT: [ReleaseChannelEnum.RELEASE, ReleaseChannelEnum.DAILY],
     ProductEnum.PACKAGE_MANAGER: [ReleaseChannelEnum.RELEASE, ReleaseChannelEnum.PREVIEW, ReleaseChannelEnum.DAILY],
     ProductEnum.WORKBENCH: [ReleaseChannelEnum.RELEASE, ReleaseChannelEnum.DAILY],
+    ProductEnum.POSITRON: [ReleaseChannelEnum.DAILY],
 }
 
 PRODUCT_RELEASE_STREAM_SUPPORT_MAP = PRODUCT_RELEASE_CHANNEL_SUPPORT_MAP  # deprecated alias, remove in Phase 0.5c
