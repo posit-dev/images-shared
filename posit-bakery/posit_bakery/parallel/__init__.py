17435aa715302a1bc76ebf8bcc7c5a5922d54592
Ian H. Pittwood <ian.pittwood@posit.co>
2026-06-30 14:26:20 -0600

    feat(parallel): add ShellJob/JobResult/CommandRunner/run_jobs multi-step jobs

--- posit-bakery/posit_bakery/parallel/__init__.py
+++ posit-bakery/posit_bakery/parallel/__init__.py
@@ -1,12 +1,18 @@
 from posit_bakery.parallel.executor import (
+    CommandRunner,
+    JobResult,
     ParallelShellExecutor,
+    ShellJob,
     ShellResult,
     ShellTask,
     resolve_max_workers,
 )
 
 __all__ = [
+    "CommandRunner",
+    "JobResult",
     "ParallelShellExecutor",
+    "ShellJob",
     "ShellResult",
     "ShellTask",
     "resolve_max_workers",
